ecf42f1bfde2d8f77855b9b2dad091cece8af185
acentelles <centelles.alberto@gmail.com>
2025-07-01 15:10:37 +0100

    Add virtualinstructionsequence trait

--- jolt-core/src/jolt_onnx/instruction/mod.rs
+++ jolt-core/src/jolt_onnx/instruction/mod.rs
@@ -1,5 +1,26 @@
 //! This module provides the custom jolt instructions for the ONNX runtime.
 
+use crate::jolt_onnx::common::onnx_trace::{ONNXInstruction, ONNXTraceRow};
+
+pub trait VirtualInstructionSequence {
+    const SEQUENCE_LENGTH: usize;
+    fn virtual_sequence(instruction: ONNXInstruction) -> Vec<ONNXInstruction> {
+        let dummy_trace_row = ONNXTraceRow {
+            instruction,
+            layer_state: LayerState {
+                input_vals: None,
+                output_vals: None,
+            },
+        };
+        Self::virtual_trace(dummy_trace_row)
+            .into_iter()
+            .map(|trace_row| trace_row.instruction)
+            .collect()
+    }
+    fn virtual_trace(trace_row: ONNXTraceRow) -> Vec<ONNXTraceRow>;
+    fn sequence_output(x: u64, y: u64) -> u64;
+}
+
 pub mod max;
 pub mod relu;
 pub mod sigmoid;
