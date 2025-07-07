98a2ad606c8643845a821d4cf26e4d6383d2dc0f
acentelles <centelles.alberto@gmail.com>
2025-07-07 18:23:29 +0100

    Add lookup method

--- jolt-core/src/jolt_onnx/instruction/mod.rs
+++ jolt-core/src/jolt_onnx/instruction/mod.rs
@@ -3,6 +3,14 @@
 use crate::jolt_onnx::common::onnx_trace::{LayerState, ONNXInstruction, ONNXTraceRow};
 use crate::{field::JoltField, jolt::instruction::JoltInstruction, jolt_onnx::tracer::tensor::QuantizedTensor};
 
+pub mod max;
+pub mod relu;
+pub mod sigmoid;
+pub mod div;
+pub mod virtual_advice;
+pub mod virtual_assert_valid_div0;
+pub mod virtual_assert_valid_signed_remainder;
+pub mod test;
 pub trait VirtualInstructionSequence {
     const SEQUENCE_LENGTH: usize;
     fn virtual_sequence(instruction: ONNXInstruction) -> Vec<ONNXInstruction> {
@@ -23,14 +31,6 @@ pub trait VirtualInstructionSequence {
     fn sequence_output(x: i8, y: i8) -> i8;
 }
 
-pub mod max;
-pub mod relu;
-pub mod sigmoid;
-pub mod div;
-pub mod virtual_advice;
-pub mod virtual_assert_valid_div0;
-pub mod virtual_assert_valid_signed_remainder;
-pub mod test;
 
 /// Trait for ONNX instructions.
 pub trait JoltONNXInstruction<InnerInstruction: JoltInstruction> {
@@ -39,4 +39,13 @@ pub trait JoltONNXInstruction<InnerInstruction: JoltInstruction> {
 
     /// Retrieve the inner instructions.
     fn inner_instructions(&self) -> Vec<InnerInstruction>;
+
+    fn lookup(&self) -> QuantizedTensor {
+        let mut inner_instructions = self.inner_instructions();
+        let mut results = vec![];
+        for inner_instruction in inner_instructions {
+            results.push(inner_instruction.lookup_entry() as u32);
+        }
+        self.combine_instruction_results(&results)
+    }
 }
