b6c3a4f0f86418167a6fc1caf8f95a414e9b385d
AntoineF4C5 <antoinedcht@hotmail.com>
2025-08-05 11:23:00 +0200

    fix: proposing a fix for lookup_trace

--- zkml-jolt-core/src/jolt/lookup_trace.rs
+++ zkml-jolt-core/src/jolt/lookup_trace.rs
@@ -6,23 +6,29 @@ use jolt_core::jolt::{
     lookup_table::LookupTables,
 };
 
-pub const WORD_SIZE: usize = 32;
+// pub const WORD_SIZE: usize = 32;
+
+macro_rules! test_const_generic {
+    ( $variant:ident : $inner:ident <WORD_SIZE>) => {};
+}
+
+test_const_generic!(Add: ADD<WORD_SIZE>);
 
 macro_rules! define_lookup_enum {
     (
         enum $enum_name:ident,
-        const $word_size:ident,
+     //   const $word_size:ident,
         trait $trait_name:ident,
-        $($variant:ident : $inner:ty),+ $(,)?
+        $($variant:ident : $inner:ident < WORD_SIZE >),+ $(,)?
     ) => {
         #[derive(Debug)]
-        pub enum $enum_name {
+        pub enum $enum_name<const WORD_SIZE: usize> {
             $(
-                $variant($inner),
+                $variant($inner<WORD_SIZE>),
             )+
         }
 
-        impl $trait_name<$word_size> for $enum_name {
+        impl<const WORD_SIZE: usize> $trait_name<WORD_SIZE> for $enum_name<WORD_SIZE> {
             fn to_instruction_inputs(&self) -> (u64, i64) {
                 match self {
                     $(
@@ -60,14 +66,13 @@ macro_rules! define_lookup_enum {
 
 define_lookup_enum!(
     enum ONNXLookup,
-    const WORD_SIZE,
     trait LookupQuery,
     Add: ADD<WORD_SIZE>,
     Sub: SUB<WORD_SIZE>,
     Mul: MUL<WORD_SIZE>,
 );
 
-impl InstructionLookup<WORD_SIZE> for ONNXLookup {
+impl<const WORD_SIZE: usize> InstructionLookup<WORD_SIZE> for ONNXLookup<WORD_SIZE> {
     fn lookup_table(&self) -> Option<LookupTables<WORD_SIZE>> {
         match self {
             ONNXLookup::Add(add) => add.lookup_table(),
@@ -77,12 +82,12 @@ impl InstructionLookup<WORD_SIZE> for ONNXLookup {
     }
 }
 
-pub trait LookupTrace {
-    fn to_lookup(&self) -> Option<ONNXLookup>;
+pub trait LookupTrace<const WORD_SIZE: usize> {
+    fn to_lookup(&self) -> Option<ONNXLookup<WORD_SIZE>>;
 }
 
-impl LookupTrace for ONNXCycle {
-    fn to_lookup(&self) -> Option<ONNXLookup> {
+impl<const WORD_SIZE: usize> LookupTrace<WORD_SIZE> for ONNXCycle {
+    fn to_lookup(&self) -> Option<ONNXLookup<WORD_SIZE>> {
         match self.instr.opcode {
             ONNXOpcode::Add => Some(ONNXLookup::Add(ADD(self.ts1_val(), self.ts2_val()))),
             ONNXOpcode::Sub => Some(ONNXLookup::Sub(SUB(self.ts1_val(), self.ts2_val()))),
