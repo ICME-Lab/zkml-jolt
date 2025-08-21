537dede6be5d18fdeaa26bde75c7f6e895ce9707
AntoineF4C5 <antoinedcht@hotmail.com>
2025-08-21 19:14:06 +0200

    clean: removed unused helper

--- zkml-jolt-core/src/utils/mod.rs
+++ zkml-jolt-core/src/utils/mod.rs
@@ -1,8 +1,4 @@
-/// Helper function to convert Vec<u64> to iterator of i128
-pub fn u64_vec_to_i128_iter(vec: &[u64]) -> impl Iterator<Item = i128> + '_ {
-    vec.iter().map(|v| *v as u32 as i32 as i64 as i128)
-}
-
+/// Helper function to convert Vec<u64> to iterator of i32
 pub fn u64_vec_to_i32_iter(vec: &[u64]) -> impl Iterator<Item = i32> + '_ {
     vec.iter().map(|v| *v as u32 as i32)
 }
