07b4b087b8f50046130a3104b5443a2bac6bff82
acentelles <centelles.alberto@gmail.com>
2025-08-22 15:23:27 +0100

    Clippy

--- zkml-jolt-core/src/jolt/tensor_heap/output_check.rs
+++ zkml-jolt-core/src/jolt/tensor_heap/output_check.rs
@@ -332,8 +332,7 @@ impl<F: JoltField, ProofTranscript: Transcript> BatchableSumcheckInstance<F, Pro
 
         // Recall that the sumcheck expression is:
         //   0 = \sum_k eq(r_address, k) * output_range(k) * (Val_final(k) - Val_output(k))
-        let result = eq_eval * output_range_eval * (*val_final_claim - val_output_eval);
-        result
+        eq_eval * output_range_eval * (*val_final_claim - val_output_eval);
     }
 }
 
