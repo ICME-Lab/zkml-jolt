dd4fe27a95e6c68f2ce36914b22e01c7ceeef258
AntoineF4C5 <antoinedcht@hotmail.com>
2025-08-22 16:29:54 +0200

    feat: start work on RebaseScale operator

--- zkml-jolt-core/src/jolt/instruction/mod.rs
+++ zkml-jolt-core/src/jolt/instruction/mod.rs
@@ -6,6 +6,7 @@ pub mod beq;
 pub mod div;
 pub mod ge;
 pub mod mul;
+pub mod rebase_scale;
 pub mod reduce_max;
 pub mod sub;
 pub mod virtual_advice;
--- zkml-jolt-core/src/jolt/instruction/rebase_scale.rs
+++ zkml-jolt-core/src/jolt/instruction/rebase_scale.rs
@@ -10,9 +10,16 @@
 //     virtual_assert_valid_signed_remainder::AssertValidSignedRemainderInstruction, JoltInstruction,
 // };
 
-use onnx_tracer::trace_types::{ONNXCycle, ONNXOpcode};
+use onnx_tracer::{
+    constants::{MAX_TENSOR_SIZE, virtual_tensor_index},
+    tensor::Tensor,
+    trace_types::{MemoryState, ONNXCycle, ONNXInstr, ONNXOpcode},
+};
 
-use crate::jolt::instruction::VirtualInstructionSequence;
+use crate::{
+    jolt::instruction::{VirtualInstructionSequence, div::DIVInstruction},
+    utils::u64_vec_to_i32_iter,
+};
 
 macro_rules! expect_rebase_scale {
     ($cycle:expr) => {
@@ -24,32 +31,88 @@ macro_rules! expect_rebase_scale {
 }
 
 /// Perform signed division and return the result
-pub struct REBASEInstruction;
+pub struct REBASEInstruction<const WORD_SIZE: usize>;
 
-impl VirtualInstructionSequence for REBASEInstruction {
+impl<const WORD_SIZE: usize> VirtualInstructionSequence for REBASEInstruction<WORD_SIZE> {
     const SEQUENCE_LENGTH: usize = 2;
 
     fn virtual_trace(cycle: ONNXCycle) -> Vec<ONNXCycle> {
         expect_rebase_scale!(cycle);
-        let mut virtual_sequence = vec![];
-        if let ONNXOpcode::RebaseScale(inner_opcode) = &cycle.instr.opcode {
-            let inner_opcode = (**inner_opcode).clone();
-            let mut instr = cycle.instr.clone();
-            instr.opcode = inner_opcode;
-            virtual_sequence.push(ONNXCycle {
-                instr,
-                memory_state: cycle.memory_state.clone(),
-                advice_value: None,
-            });
+
+        let inner_opcode = match &cycle.instr.opcode {
+            ONNXOpcode::RebaseScale(inner) => inner,
+            _ => unreachable!(),
         };
-        // TODO: Do expanded div sequence here
-        let mut div_cycle = cycle;
-        div_cycle.instr.opcode = ONNXOpcode::Div;
-        virtual_sequence.push(div_cycle);
-        virtual_sequence
+
+        // Virtual registers used in sequence
+        // TODO(AntoineF4C5): Check if conflict between this and inner DIV's virtual register also indexed to 0
+        let v_0 = Some(virtual_tensor_index(0)); // Inner operator output to be rebased 
+
+        let mut virtual_trace = vec![];
+
+        // Apply inner operator
+        let inner_opcode = (**inner_opcode).clone();
+        let mut instr = cycle.instr.clone();
+        instr.opcode = inner_opcode;
+        let inner_res_0: Vec<u64> = Vec::new(); // TODO(AntoineF4C5): Need to set to inner operator's result
+        virtual_trace.push(ONNXCycle {
+            instr: ONNXInstr {
+                address: cycle.instr.address, // TODO(AntoineF4C5): Might be more readable to have separate behavior for each possible inner opcode
+                opcode: instr.opcode,         // and only set required fields
+                ts1: cycle.instr.ts1,
+                ts2: cycle.instr.ts2,
+                ts3: cycle.instr.ts3,
+                td: v_0,
+                imm: cycle.instr.imm.clone(),
+                virtual_sequence_remaining: Some(Self::SEQUENCE_LENGTH - virtual_trace.len() - 1),
+                active_output_elements: cycle.instr.active_output_elements,
+            },
+            memory_state: MemoryState {
+                ts1_val: cycle.memory_state.ts1_val.clone(),
+                ts2_val: cycle.memory_state.ts2_val.clone(),
+                ts3_val: cycle.memory_state.ts3_val.clone(),
+                td_pre_val: cycle.memory_state.td_pre_val.clone(),
+                td_post_val: Some(Tensor::from(u64_vec_to_i32_iter(&inner_res_0))), // Needs to set to the inner operator's expected output
+            },
+            advice_value: None,
+        });
+
+        // Apply div operator by 2^scale
+        let res = DIVInstruction::<WORD_SIZE>::sequence_output(
+            inner_res_0.clone(),
+            vec![128; MAX_TENSOR_SIZE], // TODO(AntoineF4C5): Check if 0 for anything out of active_output_elements
+        );
+        virtual_trace.push(ONNXCycle {
+            instr: ONNXInstr {
+                address: cycle.instr.address,
+                opcode: ONNXOpcode::Div,
+                ts1: v_0,
+                ts2: None,
+                ts3: None,
+                td: cycle.instr.td,
+                // TODO(AntoineF4C5): Check if 0 for anything out of active_output_elements
+                // Do we need to enforce 128 denominator
+                imm: Some(Tensor::from(u64_vec_to_i32_iter(&vec![
+                    128;
+                    MAX_TENSOR_SIZE
+                ]))),
+                virtual_sequence_remaining: Some(Self::SEQUENCE_LENGTH - virtual_trace.len() - 1),
+                active_output_elements: cycle.instr.active_output_elements,
+            },
+            memory_state: MemoryState {
+                ts1_val: Some(Tensor::from(u64_vec_to_i32_iter(&inner_res_0))),
+                ts2_val: None,
+                ts3_val: None,
+                td_pre_val: None,
+                td_post_val: Some(Tensor::from(u64_vec_to_i32_iter(&res))),
+            },
+            advice_value: None,
+        });
+
+        virtual_trace
     }
 
-    fn sequence_output(_x: u64, _y: u64) -> u64 {
+    fn sequence_output(_x: Vec<u64>, _y: Vec<u64>) -> Vec<u64> {
         todo!()
     }
 }
