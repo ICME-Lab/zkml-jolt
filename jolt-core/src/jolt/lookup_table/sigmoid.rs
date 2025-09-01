a08e474f5a1b9a2753fab4ea0fee08d32ba0665e
acentelles <centelles.alberto@gmail.com>
2025-09-01 22:43:13 +0100

    Use approximate sigmoid table

--- jolt-core/src/jolt/lookup_table/sigmoid.rs
+++ jolt-core/src/jolt/lookup_table/sigmoid.rs
@@ -22,16 +22,29 @@ pub const SIGMOID_SCALED_TABLE: [u8; LUT_SIZE] = [
     7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
 ];
 
+pub const APPROXIMATE_SIGMOID_SCALED_TABLE: [u8; LUT_SIZE] = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2,
+    2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5,
+    5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7,
+    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+];
+
 #[derive(Copy, Clone, Default, Debug, Serialize, Deserialize, PartialEq)]
 pub struct SigmoidTable<const WORD_SIZE: usize>;
 
 impl<const WORD_SIZE: usize> JoltLookupTable for SigmoidTable<WORD_SIZE> {
     fn materialize_entry(&self, index: u64) -> u64 {
-        if index < (LUT_SIZE / 2) as u64 {
-            SIGMOID_SCALED_TABLE[index as usize + LUT_SIZE / 2] as u64
-        } else if index > u64::MAX - (LUT_SIZE / 2) as u64 {
-            SIGMOID_SCALED_TABLE[LUT_SIZE / 2 - (index as u8 as usize) - 1] as u64
-        } else if index < u64::MAX / 2 {
+        let max = 1 << WORD_SIZE;
+        let i = index % max;
+        if i < (LUT_SIZE / 2) as u64 {
+            APPROXIMATE_SIGMOID_SCALED_TABLE[i as usize + LUT_SIZE / 2] as u64
+        } else if i > max - (LUT_SIZE / 2) as u64 {
+            let diff = max - i;
+            APPROXIMATE_SIGMOID_SCALED_TABLE[(LUT_SIZE / 2 - diff as usize) - 1] as u64
+        } else if i < max / 2 {
             SCALE as u64
         } else {
             0
@@ -40,48 +53,42 @@ impl<const WORD_SIZE: usize> JoltLookupTable for SigmoidTable<WORD_SIZE> {
 
     fn evaluate_mle<F: JoltField>(&self, r: &[F]) -> F {
         debug_assert_eq!(r.len(), 2 * WORD_SIZE);
+        println!("r = {:?}", r);
 
-        let is_neg = r[WORD_SIZE];
+        let is_neg = r[0];
 
         let mut is_small = F::one();
         // We only consider the first 4 bits of the input to sigmoid
-        for i in 0..WORD_SIZE - 1 - 4 {
-            is_small *= F::one() - r[WORD_SIZE - 1 - 4 - i];
+        for i in 0..2 * WORD_SIZE - 1 - 3 {
+            is_small *= F::one() - r[i];
         }
 
-        let is_big = F::one();
-        for i in 0..WORD_SIZE - 1 - 4 {
-            is_big *= r[WORD_SIZE - 1 - 4 - i];
+        let mut is_big = F::one();
+        for i in 0..2 * WORD_SIZE - 1 - 3 {
+            is_big *= r[i];
         }
 
-        let mut index = F::zero();
-        for i in 0..4 {
-            index += F::from_u64(1 << i) * r[WORD_SIZE - 1 - i];
-        }
-
-        F::from_u8(SIGMOID_SCALED_TABLE[SIGMOID_SCALED_TABLE.len() / 2 - index as usize - 1]) * is_big * is_neg 
-        + F::from_u8(SIGMOID_SCALED_TABLE[index as usize]) * is_small * (F::one() - is_neg)
-        // // TODO: We only need the first half (only one input to sigmoid)
-        // // let r_prime = &r[..r.len() / 2];
-        // let r_prime = r;
-        // let len = 1 << r_prime.len();
-        // let max = F::from_u8(SCALE as u8);
-        // let zero = F::zero();
-        // let mut f_eval = vec![max; len as usize / 2];
-        // let f_eval_r = vec![zero; len as usize / 2];
-        // f_eval.extend_from_slice(&f_eval_r);
-        // for i in 0..LUT_SIZE / 2 {
-        //     f_eval[i] = F::from_u8(SIGMOID_SCALED_TABLE[i + LUT_SIZE / 2]);
-        //     f_eval[len as usize - i - 1] = F::from_u8(SIGMOID_SCALED_TABLE[LUT_SIZE / 2 - i - 1]);
+        let mut pos_value = F::from_u8(4);
+        // The first two bits don't affect the result of sigmoid
+        // for i in 0..2 {
+        //     index += F::from_u64(1 << i) * r[WORD_SIZE - 1 - i];
         // }
 
-        // // TODO: EqPolynomial::evals(r) is too slow
-        // let eq_evals = EqPolynomial::evals(r_prime);
-        // f_eval
-        //     .iter()
-        //     .zip_eq(eq_evals.iter())
-        //     .map(|(x, e)| *x * e)
-        //     .sum()
+        for i in 2..4 {
+            pos_value += r[2 * WORD_SIZE - 1 - i];
+        }
+
+        let mut neg_value = F::from_u8(3);
+        for i in 2..4 {
+            neg_value -= r[2 * WORD_SIZE - 1 - i];
+        }
+
+        // println!("neg_value = {:?}", neg_value);
+        // println!("pos_value = {:?}", pos_value);
+        // println!("is_big = {:?}", is_big);
+        // println!("is_small = {:?}", is_small);
+        // println!("is_neg = {:?}", is_neg);
+        neg_value * is_big * is_neg + pos_value * is_small * (F::one() - is_neg) + F::from_u8(SCALE as u8) * (F::one() - is_small) * (F::one() - is_neg)
     }
 }
 
